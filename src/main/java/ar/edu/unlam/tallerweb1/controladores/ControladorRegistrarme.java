5f8bbbeeabb19c2e9fb803d28e0a6c2c8d1c8d0b
Matias <matiasalarcon55@hotmail.com>
2021-09-23 18:02:04 -0300

    Primera clase terminada

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorRegistrarme.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorRegistrarme.java
@@ -1,7 +1,27 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
-public class ControladorRegistrarme {
-    public void registrarUsuario(DatosRegistro datos) {
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
 
+public class ControladorRegistrarme {
+    public ModelAndView registrarUsuario(DatosRegistro datos) {
+        ModelMap model=new ModelMap();
+        String view;
+        String msg;
+        if(validarEmail(datos.getEmail())){
+            model.put("email", datos.getEmail());
+            msg= "Registro exitoso";
+            view="login";
+        }
+        else {
+            msg="Registro fallido";
+            view="registrarme";
+        }
+        model.put("msg",msg);
+        return new ModelAndView(view, model);
+    }
+
+    private boolean validarEmail(String email) {
+        return email.endsWith(".com") && email.contains("@");
     }
 }
--- src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorRegistrarmeTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorRegistrarmeTest.java
@@ -1,26 +1,40 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
 import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+import static org.assertj.core.api.Assertions.*;
 
 public class ControladorRegistrarmeTest {
 
     private ControladorRegistrarme controladorRegistrarme=new ControladorRegistrarme();
     private static final DatosRegistro USUARIO = new DatosRegistro("usuario@correo.com","12345","12345");
+    private static final DatosRegistro USUARIO_CON_MAIL_INCORRECTO = new DatosRegistro("correo.com","12345","12345");
 
     @Test
     public void puedoRegistrarmeConUsuarioNuevoYClaveCorrecta(){
-        givenQueElUsuarioNoExiste(USUARIO);
-        whenRegistroElUsuario(USUARIO);
-        thenElRegistroEsExitoso();
+        ModelAndView mav=whenRegistroElUsuario(USUARIO);
+        thenElRegistroEsExitoso(mav);
     }
 
-    private void givenQueElUsuarioNoExiste(DatosRegistro usuario) {
+    @Test
+    public void noPuedoRegistrarmeConMailDeUsuarioIncorrecto(){
+        ModelAndView mav=whenRegistroElUsuario(USUARIO_CON_MAIL_INCORRECTO);
+        thenElRegistroFalla(mav);
     }
 
-    private void whenRegistroElUsuario(DatosRegistro datos) {
-        controladorRegistrarme.registrarUsuario(datos);
+    private ModelAndView whenRegistroElUsuario(DatosRegistro datos) {
+        return controladorRegistrarme.registrarUsuario(datos);
     }
 
-    private void thenElRegistroEsExitoso() {
+    private void thenElRegistroEsExitoso(ModelAndView mav) {
+        assertThat(mav.getViewName()).isEqualTo("login");
+        assertThat(mav.getModel().get("msg")).isEqualTo("Registro exitoso");
     }
+
+    private void thenElRegistroFalla(ModelAndView mav) {
+        assertThat(mav.getViewName()).isEqualTo("registrarme");
+        assertThat(mav.getModel().get("msg")).isEqualTo("Registro fallido");
+    }
+
+
 }
